6a9cec0458e7f074a8f43846d78cc920d6130a92
Hannes Kunstreich <hannes@kunstreich.name>
2012-06-14 17:09:34 +0200

    added hashgram modifications to get scrolling away on 13"

---
+++ hashgr.am.js
@@ -0,0 +1,17 @@
+$("#logo").css({
+  "display": "none",
+});
+$(".search").css({
+	"position": "relative",
+	"top": "-106px",
+	"margin": "0",
+	"left": "20px"
+});
+$("#media").css({
+  "position": "relative",
+  "top": "-218px"
+});
+
+
+
+
---
+++ luxogram.co.js
@@ -0,0 +1,3 @@
+$("#beta").css({
+  "display": "none",
+});
