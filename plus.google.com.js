e46bb840898bb1264e0fb6a5f1a7c7788556aa53
Hannes Kunstreich <hannes@kunstreich.name>
2012-06-14 17:08:32 +0200

    added hashgram modifications to get scrolling away on 13"

--- plus.google.com.js
+++ plus.google.com.js
@@ -3,3 +3,15 @@
 $("body").css({
   "font-family": "'helvetica neue',helvetica,sans-serif",
 });
+$("#contentPane .a-Wf-i-M").css({
+  "width": "574px",
+  "margin-left": "188px"
+});
+$("a#gbg1").css({
+  "position": "fixed",
+  "background-color"; "inherit"
+});
+$("ol.gbtc > li.gbtc:first-child").css({
+  "margin-right": "34px"
+});
+
